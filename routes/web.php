661b801d91eb287c30322e11ff05464f66cbd2b4
zgf123 <277845885@qq.com>
2018-06-21 14:49:11 +0000

    topic list

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
