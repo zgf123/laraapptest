42e66257731d9f5f0a1f20384fed82b682835a1a
zgf123 <277845885@qq.com>
2018-06-22 18:23:32 +0000

    redis horizon queue

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,7 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -15,9 +15,11 @@ class TopicObserver
 
         $topic->excerpt = make_excerpt($topic->body);
         
+    }
+
+    public function saved(Topic $topic){
         if( empty($topic->slug) ){
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            dispatch( new TranslateSlug($topic) );
         }
-        
     }
 }
--- readme.md
+++ readme.md
@@ -35,3 +35,8 @@ php artisan vendor:publish --provider="Mews\Purifier\PurifierServiceProvider"
 
 composer require "guzzlehttp/guzzle:~6.3"
 composer require "overtrue/pinyin:~3.0"
+
+
+composer require "predis/predis:~1.0"
+composer require "laravel/horizon:~1.0"
+php artisan vendor:publish --provider="Laravel\Horizon\HorizonServiceProvider"
