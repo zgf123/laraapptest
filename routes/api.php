0018249d63a160dda99d6342f358460df8937f6b
zgf123 <277845885@qq.com>
2018-07-01 16:44:09 +0000

    easy sms conf

--- routes/api.php
+++ routes/api.php
@@ -15,14 +15,8 @@ use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', function($api) {
-    $api->get('version', function() {
-        return response('this is version v1');
-    });
-});
-
-$api->version('v2', function($api) {
-    $api->get('version', function() {
-        return response('this is version v2');
-    });
+$api->version('v1',[
+    'namespace' => 'App\Http\Controllers\Api'
+], function($api) {
+    $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
 });
