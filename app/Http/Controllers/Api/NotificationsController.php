cd930c7c2d04ee9f47824b0ae463d34de8763b77
zgf123 <277845885@qq.com>
2018-07-30 01:39:34 +0000

    notifications markAsRead

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -18,4 +18,10 @@ class NotificationsController extends Controller
             'unread_count' => $this->user()->notification_count,
         ]);
     }
+
+    public function read(){
+        $this->user()->markAsRead();
+        
+        return $this->response->noContent();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -81,6 +81,8 @@ $api->version('v1',[
             $api->get('user/notifications', 'NotificationsController@index')->name('api.user.notifications.index');
             //通知统计
             $api->get('user/notifications/stats', 'NotificationsController@stats')->name('api.user.notifications.stats');
+            //标记消息通知为已读
+            $api->patch('user/read/notifications', 'NotificationsController@read')->name('api.user.notifications.read');
         });
     });
 });
